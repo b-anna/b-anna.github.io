5074d248597765a892d536a6221e8de03332ef37
Anna <boyko95boyko@gmail.com>
2017-08-03 20:00:31 +0300

    Add files via upload

---
+++ final exam/gulpfile.js
@@ -0,0 +1,122 @@
+"use strict";
+let  gulp = require("gulp");
+let concat = require("gulp-concat");
+let rigger = require("gulp-rigger");
+let autoprefixer = require("gulp-autoprefixer");
+let browserSync = require("browser-sync");
+let cssnano = require("gulp-cssnano");
+let del = require("del");
+let uglify = require("gulp-uglify");
+let babel = require("gulp-babel");
+let imagemin = require("gulp-imagemin");
+let pngquant = require("imagemin-pngquant");
+
+
+let paths = {
+  src: {
+    html: "src/*.html",
+    css: "src/sass/**/*.css",
+    js: "src/js/**/*.js",
+    fonts: "src/fonts/**/*.*",
+    img: "src/img/**/*.+(png|jpg|gif|svg)"
+  },
+  dist: {
+    html: "dist",
+    css: "dist/css",
+    js: "dist/js",
+    fonts: "dist/fonts",
+    img: "dist/img",
+  },
+  watch: {
+    html: "src/**/*.html"
+  },
+  clean: "./dist"
+};
+
+
+let serverConfig = {
+  server: {
+    baseDir: "./dist"
+  },
+  host: "localhost",
+  port: 9000,
+  logPrefix: "NASA",
+  notify: false
+};
+
+
+gulp.task("bundleHtml", function() {
+  return gulp.src(paths.src.html)
+    .pipe(rigger())
+    .pipe(gulp.dest(paths.dist.html))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+
+gulp.task("bundleCss", function() {
+  return gulp.src(paths.src.css)
+    // .pipe(sass().on("error", sass.logError))
+    .pipe(concat("styles.css"))
+    .pipe(autoprefixer({
+      browsers: ["last 2 versions"],
+      cascade: false
+    }))
+    .pipe(cssnano())
+    .pipe(gulp.dest(paths.dist.css))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+
+gulp.task("bundleJs", function() {
+  return gulp.src(paths.src.js)
+    .pipe(concat("scripts.js"))
+    .pipe(babel({
+      presets: ["env"]
+    }))
+    .pipe(gulp.dest(paths.dist.js))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+gulp.task("bundleFonts", function() {
+  return gulp.src(paths.src.fonts)
+    .pipe(gulp.dest(paths.dist.fonts));
+});
+
+gulp.task("bundleImg", function() {
+  return gulp.src(paths.src.img)
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{
+        removeViewBox: false
+      }],
+      use: [pngquant()],
+      interlaced: true
+    }))
+    .pipe(gulp.dest(paths.dist.img))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+gulp.task("webServer", function() {
+  browserSync(serverConfig);
+});
+
+gulp.task("watch", function() {
+  gulp.watch(paths.src.css, ["bundleCss"]);
+  gulp.watch(paths.watch.html, ["bundleHtml"]);
+  gulp.watch(paths.src.js, ["bundleJs"]);
+  gulp.watch(paths.src.fonts, ["bundleFonts"]);
+  gulp.watch(paths.src.img, ["bundleImg"]);
+});
+gulp.task("clean:dist", function() {
+  return del.sync(paths.clean);
+});
+
+gulp.task("start", ["clean:dist", "bundleHtml", "bundleCss", "bundleJs", "bundleFonts", "bundleImg", "webServer", "watch"]);
